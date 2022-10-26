1ef234a0ca193c4e7194674fdbfab3fa20e88921
okestro <woorong98@gmail.com>
2022-10-26 21:15:58 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulweb/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.restfulweb;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    //GET
+    // /hello-world (endpoint)
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HellowWorldBean hellowWorldBean() {
+        return new HellowWorldBean("Hello World"); //json 형태로 반환해준다. responsebody에 담지 않더라도
+    }
+}
---
+++ src/main/java/com/example/restfulweb/HellowWorldBean.java
@@ -0,0 +1,13 @@
+package com.example.restfulweb;
+//lombok
+
+import lombok.AllArgsConstructor;
+import lombok.Data; //setter 와 getter 를 사용하지 않아도 된다.
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor //생성자 만들어주는 어노테이션
+@NoArgsConstructor
+public class HellowWorldBean {
+    private String message;
+}
